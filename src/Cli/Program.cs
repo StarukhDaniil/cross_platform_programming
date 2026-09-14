3847677ab74ce59990cea831628122a1191d294d
StarukhDaniil <starukh.danil@gmail.com>
2026-09-14 17:16:08 +0300

    feat: add program and readme

--- src/Cli/Program.cs
+++ src/Cli/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Runtime.InteropServices;
+Console.WriteLine("CrossApp – практикум з крос-платформного програмування");
+Console.WriteLine("Студент: Старух Даніїл, ФЕІ-35");
+Console.WriteLine(new string('-', 52));
+Console.WriteLine($"ОС (OSDescription) : {RuntimeInformation.OSDescription}");
+Console.WriteLine($"ОС (Environment) : {Environment.OSVersion}");
+Console.WriteLine($"Архітектура процесу : {RuntimeInformation.ProcessArchitecture}");
+Console.WriteLine($"Версія .NET (CLR) : {Environment.Version}");
+Console.WriteLine($"Runtime : {RuntimeInformation.FrameworkDescription}");
+Console.WriteLine($"Каталог застосунку : {AppContext.BaseDirectory}");
+Console.WriteLine($"Поточний каталог : {Environment.CurrentDirectory}");
+Console.WriteLine(new string('-', 52));
+Console.WriteLine("Предметна область: бібліотека");
